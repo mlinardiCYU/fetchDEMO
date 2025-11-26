20e2bab2b6737d63f190377b1414e4b93b38bfd3
Michele Linardi <mlinardi@ADMINISTRATION.ADM>
2025-11-26 12:08:16 +0100

    update

--- fetch.js
+++ fetch.js
@@ -1,7 +1,7 @@
 async function afficherMeteo() {
-    const reponse = await fetch("https://api.tutiempo.net/json/?lan=fr&apid=zwDX4azaz4X4Xqs&ll=40.4178,-3.7022");
-    const meteoData = await reponse.json();
-    console.log(meteoData);
+    const reponse = await fetch("./festivals-val-d'oise.json");
+    const festival = await reponse.json();
+    console.log(festival);
   }
 
 afficherMeteo();

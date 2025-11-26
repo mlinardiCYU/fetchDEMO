4a642138adbafbe3bfcca9cd320afc578a6c468c
Michele Linardi <mlinardi@ADMINISTRATION.ADM>
2025-11-26 12:14:54 +0100

    update example

--- fetch.js
+++ fetch.js
@@ -2,6 +2,8 @@ async function afficherMeteo() {
     const reponse = await fetch("./festivals-val-d'oise.json");
     const festival = await reponse.json();
     console.log(festival);
+    console.log(festival);
+    console.log("Nom du premier festival: "+festival[0].nom_du_festival);
   }
 
 afficherMeteo();

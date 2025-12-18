9375380320becef502be6dbaf48ec3343474978e
mlinardiCYU <114984447+mlinardiCYU@users.noreply.github.com>
2025-12-18 12:05:54 +0100

    add console.log country log

--- getCountryByNameInfo.js
+++ getCountryByNameInfo.js
@@ -2,12 +2,12 @@ async function getLatLongCountryByName(name = 'France') {
   const url = `https://restcountries.com/v3.1/name/${name}`;
   
   try {
-    
+
     const response = await fetch(url);
     if (!response.ok) throw new Error('Network response not ok');
     const data = await response.json();
     console.log("lat long Cypre: "+data[0].latlng);
-
+    console.log("data complete Cypre: ", data[0]);
   } catch (error) {
     console.error('Fetch error:', error);
   }

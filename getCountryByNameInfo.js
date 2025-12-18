073b10432777ac0f459c0fdb4f3fd9af7dfb7ec8
mlinardiCYU <114984447+mlinardiCYU@users.noreply.github.com>
2025-12-18 12:03:16 +0100

    fix erreur API country

--- getCountryByNameInfo.js
+++ getCountryByNameInfo.js
@@ -1,16 +1,17 @@
-async function getCountryByName(name = 'France') {
+async function getLatLongCountryByName(name = 'France') {
   const url = `https://restcountries.com/v3.1/name/${name}`;
   
   try {
+    
     const response = await fetch(url);
     if (!response.ok) throw new Error('Network response not ok');
     const data = await response.json();
-    return data;
+    console.log("lat long Cypre: "+data[0].latlng);
+
   } catch (error) {
     console.error('Fetch error:', error);
   }
 }
 
 //Nous recuperons les données pour Chypre
-dataJSON = getCountryByName("Cyprus");
-console.log(dataJSON);
+getLatLongCountryByName("Cyprus");

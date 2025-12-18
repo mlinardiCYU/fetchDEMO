eddc390ee24495ca4461d99aa47c155133a4fd9d
mlinardiCYU <114984447+mlinardiCYU@users.noreply.github.com>
2025-12-18 11:54:42 +0100

    fix erreur API country

--- getCountryByNameInfo.js
+++ getCountryByNameInfo.js
@@ -5,8 +5,6 @@ async function getCountryByName(name = 'France') {
     const response = await fetch(url);
     if (!response.ok) throw new Error('Network response not ok');
     const data = await response.json();
-    console.log('Temperature Actuelle à Paris:', data.current_weather.temperature, '°C');
-    console.log('Vitesse du vent à Paris:', data.current_weather.windspeed, 'km/h');
     return data;
   } catch (error) {
     console.error('Fetch error:', error);
